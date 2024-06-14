578b8067af4fc857d0da4302ff28d8b89a1f97ce
toms74209200 <motomotomato@gmail.com>
2024-06-14 22:08:55 +0900

    :sparkles: Add increment countor

---
+++ widgetbook_example/lib/components/increment_counter.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+class IncrementCounter extends StatelessWidget {
+  const IncrementCounter({super.key, this.onPressed});
+
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {},
+      tooltip: 'Increment',
+      child: const Icon(Icons.add),
+    );
+  }
+}
+
+@widgetbook.UseCase(
+  name: 'Default',
+  type: IncrementCounter,
+)
+IncrementCounter defalautIncrementCounter(BuildContext context) {
+  return IncrementCounter(
+    onPressed: () {},
+  );
+}
